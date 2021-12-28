9fea40fb7f7858bb10a5ba8b7b59b3448c14f04e
Salmanim20 <83485479+Salmanim20@users.noreply.github.com>
2021-12-28 16:59:57 +0700

    Delete webcar directory

--- webcar/components/navbar.php
+++
@@ -1,8 +0,0 @@
-<header class="header black-bg">
-  <div class="sidebar-toggle-box">
-    <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
-  </div>
-  <!--logo start-->
-  <a class="logo"><b>WEB<span>CAR</span></b></a>
-  <!--logo end-->
-</header>
